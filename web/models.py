397557b75aaf6c3366742ff39dc9ba0024622092
Mohammad Ghorbani <mohammadgh.swe@gmail.com>
2025-07-30 01:45:00 +0330

    Income added in Model and some beautification

--- web/models.py
+++ web/models.py
@@ -1,11 +1,28 @@
+import datetime
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 class Expense (models.Model):
     text = models.CharField(max_length=256)
-    expenseDate = models.DateField()
+    expenseDate = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__ (self):
+        
+        return f"{self.amount} - {dateformater(self.expenseDate)}"
+
+class Income (models.Model):
+    text = models.CharField(max_length=256)
+    incomeDate = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__ (self):
+        return f"{self.amount} - {dateformater(self.incomeDate)}"
 
 
+def dateformater(date):
+    date_string = str(date)
+    datetime_object = datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S%z")
+    formatted_datetime_str = datetime_object.strftime("%Y/%m/%d %H:%M")
+    return formatted_datetime_str
